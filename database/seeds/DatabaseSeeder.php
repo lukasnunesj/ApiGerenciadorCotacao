ffde45b12e1bdbe092fc04e50180a4450366867a
Lucas Nunes <lukasnunesj@gmail.com>
2019-03-02 14:22:38 -0300

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'Lucas Nunes',
+            'email' => 'lucas@mail.com',
+            'password' => bcrypt('123456'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
